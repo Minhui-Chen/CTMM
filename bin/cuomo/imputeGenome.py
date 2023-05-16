524a7d90e611fd84b85bfd8ffc2ef817e20fa0f5
Minhui-Chen <minhui.chen.8907@hotmail.com>
2023-05-16 11:18:40 -0500

    use preprocess imputation

--- bin/cuomo/imputeGenome.py
+++ bin/cuomo/imputeGenome.py
@@ -1,8 +1,6 @@
 import os, shutil, sys
 import numpy as np, pandas as pd
-import rpy2.robjects as ro
-from rpy2.robjects import r, pandas2ri, numpy2ri
-from rpy2.robjects.packages import importr, STAP
+from ctmm import preprocess
 
 def main():
     # par
@@ -11,42 +9,21 @@ def main():
 
     # read 
     y = pd.read_table(input.y, index_col=(0,1)) # donor - day * gene
-    donors = np.unique(y.index.get_level_values('donor'))
     nu = pd.read_table(input.nu, index_col=(0,1)) # donor - day * gene
-
-    # transform from donor-day * gene to donor * gene-day
-    y = y.unstack(level=1)
-    y_columns = y.columns
-    nu = nu.unstack(level=1)
-    nu_columns = nu.columns
 
     # impute all genes together
     y_path = os.path.dirname(output.y)
     nu_path = os.path.dirname(output.nu)
     if snakemake.wildcards.im_genome in ['Y']:
         if snakemake.wildcards.im_mvn == 'N':
-            softImpute_f = 'bin/cuomo/softImpute.R'
-            softImpute_r = STAP( open(softImpute_f).read(), 'softImpute_r' )
-            pandas2ri.activate()
-            numpy2ri.activate()
-            if 'seed' not in snakemake.params.keys():
-                seed = ro.NULL
-            else:
-                seed = ro.NULL if snakemake.params.seed is None else ro.FloatVector([snakemake.params.seed])
-            ### y
-            out = softImpute_r.my_softImpute( r['as.matrix'](y), scale=ro.vectors.BoolVector([True]), seed=seed )
-            out = dict( zip(out.names, list(out)) )
-            y = pd.DataFrame(out['Y'], index=y.index, columns=y.columns)[y_columns]
-            ### nu
-            out = softImpute_r.my_softImpute( r['as.matrix'](nu), scale=ro.vectors.BoolVector([True]), seed=seed )
-            out = dict( zip(out.names, list(out)) )
-            nu = pd.DataFrame(out['Y'], index=nu.index, columns=nu.columns)[nu_columns]
+            seed = snakemake.params.get('seed', None)
+            ### softimpute y
+            y = preprocess.softimpute( y, seed=seed, scale=True )
+            ### softimpute nu
+            nu = preproces.softimpute( nu, seed=seed+10, scale=True )
         else:
             sys.exit('Impute Genome only support softImpute!\n')
 
-        # transform back from donor * gene-day to donor-day * gene
-        y = y.stack(level=1).sort_values(by=['donor','day'])
-        nu = nu.stack(level=1).sort_values(by=['donor','day'])
         y.to_csv(output.y, sep='\t')
         nu.to_csv(output.nu, sep='\t')
     else:
