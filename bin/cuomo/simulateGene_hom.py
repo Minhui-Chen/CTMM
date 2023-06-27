33daa7dcff64c78f66b7cdb57ab97a7861ceb919
Minhui-Chen <minhui.chen.8907@hotmail.com>
2023-06-26 20:48:55 -0500

    update simulation functions and change hom to free params

--- bin/cuomo/simulateGene_hom.py
+++ bin/cuomo/simulateGene_hom.py
@@ -1,5 +1,7 @@
-import os, math, re
+import os, math, re, sys
 import numpy as np, pandas as pd
+from ctmm import util
+
 
 def main():
     # select genes
@@ -27,50 +29,43 @@ def main():
                 ctnu_fs[gene] = ctnu_f
 
     #
-    rng = np.random.default_rng()
-
     out = np.load(snakemake.input.out, allow_pickle=True).item()
     cty_agg = open(snakemake.output.cty, 'w')
     P_agg = open(snakemake.output.P, 'w')
     ctnu_agg = open(snakemake.output.ctnu, 'w')
-    for gene in genes:
+    for i, gene in enumerate(genes):
         gene_idx = np.where(out['gene'] == gene)[0][0]
-        beta = out['reml']['hom']['beta']['ct_beta'][gene_idx]
+        beta = out['reml']['free']['beta']['ct_beta'][gene_idx]
 
-        P_agg.write(P_fs[gene]+'\n')
         P = np.loadtxt(P_fs[gene])
         pi = np.mean(P, axis=0)
         Pd = P-pi
         ss, C = P.shape[0], P.shape[1]
         s = (Pd.T @ Pd) / (ss-1)
         # sanity check
-        if not np.isclose(beta @ s @ beta, out['reml']['hom']['fixedeffect_vars']['ct_beta'][gene_idx]):
+        if not np.isclose(beta @ s @ beta, out['reml']['free']['fixedeffect_vars']['ct_beta'][gene_idx]):
             print( beta @ s @ beta )
-            print( out['reml']['hom']['fixedeffect_vars']['ct_beta'][gene_idx] )
+            print( out['reml']['free']['fixedeffect_vars']['ct_beta'][gene_idx] )
             sys.exit('Fixed effect variance error!\n')
 
-        # homogeneous effect
-        hom2 = out['reml']['hom']['hom2'][gene_idx]
-        if hom2 < 0:
-            hom2 = 0
-        alpha = np.outer(rng.normal(scale=math.sqrt(hom2), size=(ss)), np.ones(C))
-
-        # residual effect
-        ctnu_agg.write(ctnu_fs[gene]+'\n')
-        print(ctnu_fs[gene])
+        # simulate cty
+        hom2 = out['reml']['free']['hom2'][gene_idx]
         ctnu = pd.read_table(ctnu_fs[gene]).pivot(index='donor', columns='day', values=gene)
-        delta = rng.normal(np.zeros_like(ctnu), np.sqrt(ctnu))
 
-        # pseudobulk
-        cty = alpha + np.outer(np.ones(ss), beta) + delta
+        cty = util.sim_pseudobulk(beta, hom2, ctnu.to_numpy(), ss, C, seed=snakemake.params.seed+i)
+
+        # save
         cty_f = re.sub('/rep/', f'/rep{gene}/', snakemake.params.cty)
         os.makedirs( os.path.dirname(cty_f), exist_ok=True)
         np.savetxt(cty_f, cty)
         cty_agg.write(cty_f+'\n')
+        ctnu_agg.write(ctnu_fs[gene]+'\n')
+        P_agg.write(P_fs[gene]+'\n')
     
     cty_agg.close()
     P_agg.close()
     ctnu_agg.close()
+
 
 if __name__ == '__main__':
     main()
