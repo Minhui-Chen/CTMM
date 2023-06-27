70ac5bd180cbb7e3b660c0c1a7a0065f3231daf8
Minhui-Chen <minhui.chen.8907@hotmail.com>
2023-06-26 20:51:13 -0500

    update

--- bin/sim/ctp.remlJK.py
+++ bin/sim/ctp.remlJK.py
@@ -5,7 +5,7 @@ import rpy2.robjects as robjects
 from rpy2.robjects import r, pandas2ri, numpy2ri
 from rpy2.robjects.packages import STAP
 from ctmm import util, wald, ctp, log
-import cProfile, pstats
+
 
 def main():
     # par
@@ -20,24 +20,24 @@ def main():
     outs = [re.sub('/rep/', f'/rep{i}/', params.out) for i in batch]
     for y_f, P_f, nu_f, out_f in zip(
             [line.strip() for line in open(input.y)],
-            [line.strip() for line in open(input.P)], 
-            [line.strip() for line in open(input.nu)], 
+            [line.strip() for line in open(input.P)],
+            [line.strip() for line in open(input.nu)],
             outs
-            ):
+    ):
         log.logger.info(f'{y_f}, {P_f}, {nu_f}')
 
         # cell type number
         C = np.loadtxt(y_f).shape[1]
 
         os.makedirs(os.path.dirname(out_f), exist_ok=True)
-        
+
         # Dictionary to store output
         out = {}
 
         ## REML
-        free_reml, free_reml_wald = ctp.free_REML(y_f, P_f, nu_f, method=method,  
-                jack_knife=True, optim_by_R=optim_by_R)
-        out['reml'] = {'free':free_reml, 'wald':{'free':free_reml_wald} }
+        free_reml, free_reml_wald = ctp.free_REML(y_f, P_f, nu_f, method=method,
+                                                  jack_knife=True, optim_by_R=optim_by_R)
+        out['reml'] = {'free': free_reml, 'wald': {'free': free_reml_wald}}
 
         # save
         np.save(out_f, out)
@@ -45,6 +45,6 @@ def main():
     with open(output.out, 'w') as f:
         f.write('\n'.join(outs))
 
+
 if __name__ == '__main__':
     main()
-
